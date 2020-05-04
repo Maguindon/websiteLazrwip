d536d07ace7bbecd90b47c612fdbfc5a14e97c5d
Marc-Andre Guindon <marc-andreguindon@Marc-Andres-MacBook-Pro.local>
2020-05-04 12:21:24 -0400

    JS addClass

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -40,8 +40,10 @@
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
+            $("#logo-top").addClass("navbar-shrink");
         } else {
             $("#mainNav").removeClass("navbar-shrink");
+            $("#logo-top").removeClass("navbar-shrink");
         }
     };
     // Collapse now if page is not at top
